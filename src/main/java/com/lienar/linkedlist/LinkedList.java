dea3abd8006c5736ece17dc8ffdc0f20eed58254
prasag1 <guru.prasad@bsci.com>
2022-05-20 15:47:21 +0530

    Linked List implemenation

--- src/main/java/com/lienar/linkedlist/LinkedList.java
+++ src/main/java/com/lienar/linkedlist/LinkedList.java
@@ -4,9 +4,14 @@ public class LinkedList {
 	
 	private Node head;
 	
-	class Node {
-		int data;
-		Node next;
+	
+	public Node getHead() {
+		return head;
+	}
+
+	public class Node {
+		public int data;
+		public Node next;
 		
 		Node(int data){
 			this.data = data;
@@ -25,6 +30,8 @@ public class LinkedList {
 		return head == null;
 	}
 	
+	// Time Complexity : O(n)
+	// Space Complexity : O(1)
 	public int size() {
 		int size = 0;
 		Node q = head;
@@ -35,6 +42,8 @@ public class LinkedList {
 		return size;
 	}
 	
+	// Time Complexity : O(n)
+	// Space Complexity : O(1)
 	public void printList() {
 		Node q = head;
 		while(q != null) {
@@ -43,4 +52,81 @@ public class LinkedList {
 		}
 		System.out.println("NULL");
 	}
+	
+	// Time Complexity : O(n)
+	// Space Complexity : O(1)
+	public void addAtEnd(int data) {
+		Node node = new Node(data);
+		
+		if(head == null) {
+			head = node;
+		}else {
+			Node q = head;
+			while(q.next != null) {
+				q = q.next;
+			}
+			q.next = node;
+		}
+		
+	}
+	
+	// Time Complexity : O(1)
+	// Space Complexity : O(1)
+	public void addAfterNode(int data, Node oldNode) {
+		if(oldNode == null)
+			return;
+		Node node = new Node(data);
+		node.next = oldNode.next;
+		oldNode.next = node;
+	}
+	
+	// Time Complexity : O(n)
+	// Space Complexity : O(1)
+	public void deleteNodeByItem(int item) {
+		Node q = head;
+		Node p = null;
+		if(head == null) {
+			return;
+		}
+		if(q.data == item) {
+			head = head.next;
+			return;
+		}
+		while(q != null && q.data != item) {
+			p = q;
+			q = q.next;
+		}
+		if(q == null) {
+			return;
+		}
+		else
+		{			
+			p.next = q.next;
+		}
+	}
+	
+	public void deleteNodeByPosition(int index) {
+		if(head == null || index < 0)
+			return;
+		Node q = head;
+		Node p = null;
+		int i = 0;
+		
+		if(index == 0) {
+			head = head.next;
+			return;
+		}
+		
+		while (i < index || q != null) {
+			p = q;
+			q = q.next;
+			i++;
+		}
+		
+		
+			
+	}	
+		
+		
+	
 }
--- src/test/java/com/linear/linkedlist/LinkedListTest.java
+++ src/test/java/com/linear/linkedlist/LinkedListTest.java
@@ -15,4 +15,95 @@ public class LinkedListTest {
 		list.addAtBeg(1);
 		assertFalse(list.isEmpty());
 	}
+	
+	@Test
+	public void sizeTest() {
+		LinkedList list = new LinkedList();
+		assertTrue(list.isEmpty());
+		assertEquals(0, list.size());
+		list.addAtBeg(4);
+		assertFalse(list.isEmpty());
+		assertEquals(1, list.size());
+		list.addAtBeg(3);
+		list.addAtBeg(2);
+		list.addAtBeg(1);
+		assertEquals(4, list.size());
+	}
+	
+	@Test
+	public void printListTest() {
+		LinkedList list = new LinkedList();
+		assertTrue(list.isEmpty());
+		assertEquals(0, list.size());
+		list.addAtBeg(4);
+		assertFalse(list.isEmpty());
+		assertEquals(1, list.size());
+		list.addAtBeg(3);
+		list.addAtBeg(2);
+		list.addAtBeg(1);
+		assertEquals(4, list.size());
+		list.printList();
+	}
+	
+	@Test
+	public void addAtEndTest() {
+		LinkedList list = new LinkedList();
+		assertTrue(list.isEmpty());
+		assertEquals(0, list.size());
+		list.addAtEnd(1);
+		assertFalse(list.isEmpty());
+		assertEquals(1, list.size());
+		list.addAtEnd(2);
+		list.addAtEnd(3);
+		list.addAtEnd(4);
+		assertEquals(4, list.size());
+		list.printList();
+	}
+	
+	@Test
+	public void addAfterNode() {
+		LinkedList list = new LinkedList();
+		assertTrue(list.isEmpty());
+		assertEquals(0, list.size());
+		list.addAfterNode(5, null);
+		list.addAtEnd(5);
+		assertFalse(list.isEmpty());
+		assertEquals(1, list.size());
+		list.addAfterNode(10, list.getHead());
+		assertEquals(2, list.size());
+		list.addAfterNode(15, list.getHead().next);
+		assertEquals(3, list.size());
+		list.printList();
+	}
+	
+	@Test
+	public void deleteNodeByItemTest() {
+		LinkedList list = new LinkedList();
+		assertTrue(list.isEmpty());
+		assertEquals(0, list.size());
+		list.deleteNodeByItem(5);
+		list.addAtEnd(1);
+		list.addAtEnd(2);
+		list.addAtEnd(3);
+		list.addAtEnd(4);
+		list.addAtEnd(5);
+		list.addAtEnd(6);
+		list.addAtEnd(7);
+		list.addAtEnd(8);
+		list.addAtEnd(9);
+		list.addAtEnd(10);
+		assertFalse(list.isEmpty());
+		assertEquals(10, list.size());
+		list.printList();
+		list.deleteNodeByItem(7);
+		assertFalse(list.isEmpty());
+		assertEquals(9, list.size());
+		list.printList();
+		list.deleteNodeByItem(1);
+		list.printList();
+		list.deleteNodeByItem(10);
+		list.printList();
+		list.deleteNodeByItem(11);
+		list.printList();
+	}
 }
